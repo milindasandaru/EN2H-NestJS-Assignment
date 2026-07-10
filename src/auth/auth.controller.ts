11ad391d6cba4cee2f143f40ee0f3598d05c9f48
milindasandaru <sams.senarath@gmail.com>
2026-07-10 23:34:28 +0530

    chore: implmenting auth controller and service

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Body, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { RegisterDto } from "./dto/register.dto";
+
+@Controller('auth')
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+    ) {}
+
+    @Post('register')
+    register(@Body() dto: RegisterDto) {
+        return this.authService.register(dto);
+    }
+}
