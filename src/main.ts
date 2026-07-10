441843ae8bd722fdfe502b96f50070974673271c
milindasandaru <sams.senarath@gmail.com>
2026-07-10 11:45:05 +0530

    chore: configure files with cors and database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  // create nest application instance
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.enableCors(); // enable cors
+
+  app.setGlobalPrefix('api'); // global apiprefix
+
+  // enable global validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const port = configService.get<number>('PORT', 3000); // get port from env
+  await app.listen(port);
+
+  console.log(`application is running on: http://localhost:${port}`);
 }
+
 bootstrap();
