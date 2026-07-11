66e044d3c1da3aebd5e81dd5b3d8412498bdeb15
milindasandaru <sams.senarath@gmail.com>
2026-07-11 14:47:00 +0530

    Implmenting service CRUD

--- src/services/service.controller.ts
+++ src/services/service.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Body, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  UseGuards,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ServiceService } from './service.service';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UpdateServiceDto } from './dto/update-service.dto';
 
 @Controller('services')
 export class ServiceController {
@@ -11,5 +22,27 @@ export class ServiceController {
   @Post()
   create(@Body() dto: CreateServiceDto) {
     return this.serviceService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.serviceService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.serviceService.findOne(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateServiceDto) {
+    return this.serviceService.update(id, dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.serviceService.remove(id);
   }
 }
--- src/services/service.service.ts
+++ src/services/service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateServiceDto } from './dto/create-service.dto';
+import { UpdateServiceDto } from './dto/update-service.dto';
 
 @Injectable()
 export class ServiceService {
@@ -9,6 +10,35 @@ export class ServiceService {
   async create(dto: CreateServiceDto) {
     return this.prisma.service.create({
       data: dto,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.service.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.service.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, dto: UpdateServiceDto) {
+    return this.prisma.service.update({
+      where: {
+        id,
+      },
+      data: dto,
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.service.delete({
+      where: {
+        id,
+      },
     });
   }
 }
